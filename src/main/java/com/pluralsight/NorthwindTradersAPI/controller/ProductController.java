3eb77bb71c03cfa7f8bffd48cbbdcf34389999e3
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-12-11 16:13:54 -0500

    productDao created

---
+++ src/main/java/com/pluralsight/NorthwindTradersAPI/controller/ProductController.java
@@ -0,0 +1,24 @@
+package com.pluralsight.NorthwindTradersAPI.controller;
+
+import com.pluralsight.NorthwindTradersAPI.data.ProductDao;
+import com.pluralsight.NorthwindTradersAPI.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductDao productDao;
+
+    @RequestMapping(path = "products")
+    public List<Product> getAll() {
+        List<Product> products = productDao.getAll();
+        return products;
+    }
+
+}
